62d57b8dbad4266fd089156e598eb8a7f14393cc
Adam Mankowski <maniekmanfred69@gmail.com>
2017-04-25 23:39:03 +0200

    Add colorizing of every subject in same colors by using seed method
    Implemented colorizing of every subject using seedrandom (made by
    davidbau)

--- optivumcolorizer.js
+++ optivumcolorizer.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         OptivumColorizer
 // @namespace    http://tampermonkey.net/
-// @version      0.11
+// @version      0.12
 // @description  Makes better view of lesson plan and colorize it.
 // @author       TheQuake
 // @require      http://code.jquery.com/jquery-latest.js
+// @require      http://cdnjs.cloudflare.com/ajax/libs/seedrandom/2.4.3/seedrandom.min.js
 // @include      http://89.231.4.135/files/plan_zs1/index.html
 // @include      http://89.231.4.135/files/plan_zs1/plany/*
 // @updateURL	 https://raw.githubusercontent.com/MeQuake/OptivumColorizer/master/optivumcolorizer.js
@@ -14,51 +15,37 @@
 
 (function() {
     'use strict';
+        var seedCount = 0;
+        var subjectColors = [{}];
+        var calculateColor = function() {
+            var color = '#';
+            Math.seedrandom(seedCount);
+            for(var i = 0; i < 3; i++) {
+                color += parseInt((Math.random()*255)).toString(16);
+            }
+            return color;
+        };
         $(".l", $(this).context).each(function(index, content) {
             $("span", content).each(function(i, c) {
                 if ($(c).text().search("1/2") != -1) {
                     $("br", content).remove(); //usuwa niepotrzebne zakończenia linii
                     c.remove(); //usuwa lekcje dla grupy w której się nie znajdujemy
                 }
-                var lessonCheck = function(c, lesson) { //funkcja sprawdzająca czy w komórce znajduje się dana lekcja
-                    if ($(c).text().search(lesson) != -1) {
-                        return true;
-                    } else {
-                        return false;
+                var subjectName = $(c).text();
+                var color = "";
+                var found = false;
+                $(subjectColors).each(function(i, subject) {
+                    if(subject.name == subjectName) {
+                        color = subject.color;
+                        found = true;
                     }
-                };
-                switch (true) {
-                    case lessonCheck(c, "angielski"):
-                        $(c).css('color', 'red');
-                        break;
-                    case lessonCheck(c, "matematyka"):
-                        $(c).css('color', 'blue');
-                        break;
-                    case lessonCheck(c, "polski"):
-                        $(c).css('color', 'DarkMagenta');
-                        break;
-                    case lessonCheck(c, "godz.wych"):
-                        $(c).css('color', 'DarkGreen');
-                        break;
-                    case lessonCheck(c, "religia"):
-                        $(c).css('color', 'SkyBlue');
-                        break;
-                    case lessonCheck(c, "HIS"):
-                        $(c).css('color', 'Aquamarine');
-                        break;
-                    case lessonCheck(c, "wf"):
-                        $(c).css('color', 'BurlyWood');
-                        break;
-                    case lessonCheck(c, "adm. baz"):
-                        $(c).css('color', 'DarkOrange');
-                        break;
-                    case lessonCheck(c, "progr. aplik"):
-                        $(c).css('color', 'DarkSlateBlue');
-                        break;
-                    case lessonCheck(c, "witryny"):
-                        $(c).css('color', 'OliveDrab');
-                        break;
+                });
+                if(!found) {
+                    color = calculateColor();
+                    subjectColors.push({ name: subjectName, color: color });
+                    seedCount++;
                 }
+                $(c).css('color', color);
             });
         });
 })();
